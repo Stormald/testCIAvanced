67940b13a366373e78059a5c10848fb0f05d21e3
Thomas Gomes Da Silva <t.gds74@gmail.com>
2022-04-11 15:06:18 +0200

    Ok6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require("@actions/core")
 const github = require("@actions/github")
 
 try{
-    console.log("Hello " + core.getInput("toto"))
+    console.log("Hello " + core.getInput("name_toto"))
     console.log(github)
 }catch(error) {
     core.setFailed(error.message)
